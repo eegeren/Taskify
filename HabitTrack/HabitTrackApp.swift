55f4bc1f129468d98ac4bb6041915ac9771f6da5
Yusufege Eren <yusufege.erenn@gmail.com>
2025-07-01 12:00:51 +0300

    Initial Commit

---
+++ HabitTrack/HabitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackApp.swift
+//  HabitTrack
+//
+//  Created by Yusufege Eren on 1.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTrackTests/HabitTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackTests.swift
+//  HabitTrackTests
+//
+//  Created by Yusufege Eren on 1.07.2025.
+//
+
+import Testing
+@testable import HabitTrack
+
+struct HabitTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
